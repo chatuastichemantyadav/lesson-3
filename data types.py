10b85cb522922c0e3fb2ae1d390e47ab85f8d691
chartastichemantyadav <hemantyadav0727@gmail.com>
2025-01-01 19:23:08 +0530

    lesson 3 (magic)

---
+++ data types.py
@@ -0,0 +1,11 @@
+STUPID=5
+print("Datatype of STUPID :" ,type (STUPID))
+
+L=0.4278395078756675639765757386583932639673249834274877777777777777
+print("Datatype of L :" ,type (L))
+
+EDIAH="Tony Stark"
+print("Datatype of EDIAH :" ,type (EDIAH))
+
+OK=True
+print("Datatype of OK :" ,type (OK))
---
+++ typecasting.py
@@ -0,0 +1,7 @@
+Name="Hemant"
+Age=56
+is_student=True
+weight=56.56
+
+print("student's name is: "+Name +",Age is: "+str(Age) +"Is student: "+str(is_student)+"and weight is: "+str(weight))
+print("alot of consideration\nfat child's weight is: " ,int(weight),"\nnow you need to sit on iron bench not wodden bench")
